c7e39bff3e396995b1d399b57b59973224effdb6
truongaxin123 <truongaxin123@gmail.com>
2020-07-12 15:03:20 +0700

    edit XImage, add UnsplashXImage

--- XImage.py
+++ XImage.py
@@ -1,11 +1,8 @@
-from urllib.request import urlretrieve
-
 class XImage:
 
-    def __init__(self, name, url, info={}):
+    def __init__(self, name, url):
         self.name = name
         self.url = url
-        self.info = info
 
     def __str__(self):
         return 'XImage<{} {}>'.format(self.name, self.url)
@@ -16,5 +13,18 @@ class XImage:
     def getUrl(self):
         return self.url
 
-    def getInfo(self):
-        return self.info
+    def showInfo(self):
+        for attr, value in self.__dict__.items():
+            print(attr+': '+str(value))
+
+class UnsplashXImage(XImage):
+
+    def __init__(self, name, url, created_at, updated_at, promoted_at, width, height, description):
+        super().__init__(name, url)
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.promoted_at = promoted_at
+        self.width = width
+        self.height = height
+        self.description = description
+
